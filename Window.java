348f14dc0ada0eb44644e200b4df43259aedd292
mariaxboza <65983691+mariaxboza@users.noreply.github.com>
2020-05-26 19:22:56 -0400

    Update Window.java

--- Window.java
+++ Window.java
@@ -1,12 +1,13 @@
 import javax.swing.*;
 
-public class Window {
-
-    public void msg(String msg) {
+public class Window { // Window class to use JOption Pane
+    
+    
+    public void msg(String msg) { // Method to print the message in a window
         JOptionPane.showMessageDialog(null, msg);
     }
 
-    public int option(String[] options, String msg) {
+    public int option(String[] options, String msg) { // Method to print the array list as options with the String as well
         return JOptionPane.showOptionDialog(null, msg,// my message
                 "Click a button", // dialog box title
                 JOptionPane.DEFAULT_OPTION,
@@ -16,11 +17,11 @@ public class Window {
                 options[0]); // default option
     }
 
-    public void println(String msg) {
+    public void println(String msg) { // Print the string
         System.out.println(msg);
     }
 
-    public String input(String msg) {
+    public String input(String msg) { // Get an input on the window
         return JOptionPane.showInputDialog(msg);
     }
 
