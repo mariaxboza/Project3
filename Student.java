40961de2129fffe1030d943c68203487da5dd807
mariaxboza <65983691+mariaxboza@users.noreply.github.com>
2020-05-26 19:21:16 -0400

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,21 +1,21 @@
-public class Student {
+public class Student { // Class to create the Student Object 
 
     private String FName;
     private String LName;
 
     public Student (String fName, String lName)
-    {
+    { // Constuctor to create the Student with a first name and last name
         FName = fName;
         LName = lName;
     }
 
     public String getFirstName ()
-    {
+    { // Method to get the students first name
         return FName;
     }
 
     public String getLastName()
-    {
+    {   // Method to get the students last name
         return LName;
     }
 }
